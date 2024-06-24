9ab6c3907291b26cd5f821532222a8721b6ddad7
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-06-24 10:45:20 +0100

    nextui install and setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+import {nextui} from '@nextui-org/react';
 
 const config: Config = {
+  // tells tailwind where to go and find the classNames to apply
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +18,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
