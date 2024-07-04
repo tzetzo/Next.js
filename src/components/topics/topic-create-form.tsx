1cdbb79916d52ae40da59d52344e28ed24315515
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-07-04 14:37:55 +0100

    show spinner when creating topic

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -11,6 +11,7 @@ import {
 import { useFormState } from "react-dom";
 
 import * as actions from "@/actions";
+import FormButton from "@/components/common/form-button";
 
 export default function TopicCreateForm() {
   const [formState, action] = useFormState(actions.createTopic, { errors: {} });
@@ -43,7 +44,7 @@ export default function TopicCreateForm() {
 
             {formState.errors._form ? <div className='rounded p-2 bg-red-200 border border-red-400'>{formState.errors._form?.join(', ')}</div> : null }
 
-            <Button type="submit">Submit</Button>
+            <FormButton>Save</FormButton>
           </div>
         </form>
       </PopoverContent>
