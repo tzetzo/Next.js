752e156ecee062c72e2b557b9d946214ba5ae169
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-07-10 13:58:38 +0100

    fetch the top 5 posts

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -3,20 +3,19 @@ import { db } from "@/db";
 
 // type alias extends type alias
 // manually writing the type of data returned by our query
-export type PostWithData = (
-    Post & {
-        topic: {slug: string};
-        user: {name: string | null};
-        _count: {comments: number}
-    }
-)
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
 // OR as alternative if we dont want to write it manually
 // export type PostWithData = Awaited<
 //   ReturnType<typeof fetchPostsByTopicSlug>
 // >[number];
 
-export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]>{ // goes with the first type alias above!!!
-// export function fetchPostsByTopicSlug(slug: string) {
+export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+  // goes with the first type alias above!!!
+  // export function fetchPostsByTopicSlug(slug: string) {
   return db.post.findMany({
     where: {
       topic: { slug },
@@ -26,5 +25,23 @@ export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]>{ //
       user: { select: { name: true } },
       _count: { select: { comments: true } },
     },
+  });
+}
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: {
+          _count: "desc",
+        },
+      },
+    ],
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+    take: 5
   });
 }
